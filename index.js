44283be5a8f536172511c2c2d08f05a02ff9e314
Trever Andersen <trever.r.andersen@gmail.com>
2018-02-01 23:09:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+class Driver {
+  constructor(name, started) {
+    this.name = name
+    this.started = started
+  }
 
+  startDate() {
+    let started = new Date();
+    return started
+  }
+  yearsExperienceFromBeginningOf(year) {
+    return year - startDate();
+  }
+}
+
+class Route {
+  constructor(horizontal, vertical) {
+    this.horizontal = horizontal
+    this.vertical = vertical
+  }
+}
